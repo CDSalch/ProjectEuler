094b25e99be84baff74d139dfb8f6d97fc7c749d
CDSalch <charlesdaniel.strasbourg@gmail.com>
2025-12-21 12:03:05 +0100

    feat : fix problem 3

--- 003.py
+++ 003.py
@@ -1,17 +1,20 @@
 def prime_factors(n):
     if n < 2:
-        factors = []
-        divisor = 2
+        return []  # Aucun facteur premier pour n < 2
 
-        while divisor * divisor <= n:
-            while n % divisor == 0:
-                factors.append(divisor)
-                n //= divisor
-            divisor += 1
+    factors = []
+    divisor = 2
 
-        if n > 1:
-            factors.append(n)
+    while divisor * divisor <= n:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
 
-        return factors
+    if n > 1:
+        factors.append(n)
+
+    return factors
+
 
 print(prime_factors(600851475143)[-1])
